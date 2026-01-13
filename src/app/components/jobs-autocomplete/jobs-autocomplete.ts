9e941d5c1396d43379cfeb516a42985e6e88dc20
lavaskin@gmail.com <lavaskin@gmail.com>
2026-01-13 14:35:55 -0600

    add jobs autocomplete component

---
+++ src/app/components/jobs-autocomplete/jobs-autocomplete.ts
@@ -0,0 +1,30 @@
+import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { JobSelectOptions } from '@app/data/select-options.data';
+import { SelectOptionModel } from '@app/models/select-option.model';
+import { AutoCompleteModule } from 'primeng/autocomplete';
+
+@Component({
+	selector: 'mrt-jobs-autocomplete',
+	imports: [FormsModule, AutoCompleteModule],
+	templateUrl: './jobs-autocomplete.html',
+	styleUrl: './jobs-autocomplete.scss',
+})
+export class JobsAutocomplete {
+	@Input() ngModel?: number;
+	@Output() ngModelChange: EventEmitter<number> = new EventEmitter<number>();
+
+	public jobOptions = JobSelectOptions;
+	public filteredOptions: SelectOptionModel[] = [];
+
+	public onModelChange(value?: number): void {
+		this.ngModelChange.emit(value);
+	}
+
+	public filterJobs(event: any): void {
+		const query = event.query.toLowerCase();
+		this.filteredOptions = JobSelectOptions.filter(
+			(job) => job.label?.toLowerCase().includes(query)
+		);
+	}
+}
