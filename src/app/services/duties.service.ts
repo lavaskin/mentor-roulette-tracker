514f1a2f19e900288a61b9e22dc03c56e5139711
lavaskin@gmail.com <lavaskin@gmail.com>
2026-01-12 22:17:36 -0600

    rename services to be standardized

--- src/app/services/duties.service.ts
+++ src/app/services/duties.service.ts
@@ -9,22 +9,23 @@ export class DutiesService {
 
 	private _http: HttpClient = inject(HttpClient);
 
-	public getDuties(): Observable<DutyModel[]> {
+	public getAll(): Observable<DutyModel[]> {
 		return this._http.get<DutyModel[]>(`${this._baseUrl}`);
 	}
 
-	public getDutyById(dutyId: number): Observable<DutyModel> {
+	public getById(dutyId: number): Observable<DutyModel> {
 		return this._http.get<DutyModel>(`${this._baseUrl}/${dutyId}`);
 	}
 
-	public createDuty(duty: DutyModel): Observable<DutyModel> {
+	public create(duty: DutyModel): Observable<DutyModel> {
 		return this._http.post<DutyModel>(`${this._baseUrl}`, duty);
 	}
-	public updateDuty(duty: DutyModel): Observable<DutyModel> {
+	
+	public update(duty: DutyModel): Observable<DutyModel> {
 		return this._http.put<DutyModel>(`${this._baseUrl}/${duty.dutyId}`, duty);
 	}
 
-	public deleteDuty(dutyId: number): Observable<void> {
+	public delete(dutyId: number): Observable<void> {
 		return this._http.delete<void>(`${this._baseUrl}/${dutyId}`);
 	}
 }
--- src/app/services/mentor-roulette-log.service.ts
+++ src/app/services/mentor-roulette-log.service.ts
@@ -1,11 +1,31 @@
 import { HttpClient } from "@angular/common/http";
 import { inject } from "@angular/core";
+import { MentorRouletteLogModel } from "@app/models/mentor-roulette-log.model";
 import { environment } from "environments/environment";
+import { Observable } from "rxjs";
 
 export class MentorRouletteLogService {
 	private _baseUrl: string = `${environment.apiBaseUrl}/mentorroulette`;
 
 	private _http: HttpClient = inject(HttpClient);
 
-	// ...
+	public getAll(): Observable<MentorRouletteLogModel[]> {
+		return this._http.get<MentorRouletteLogModel[]>(`${this._baseUrl}`);
+	}
+
+	public getById(mentorRouletteLogId: number): Observable<MentorRouletteLogModel> {
+		return this._http.get<MentorRouletteLogModel>(`${this._baseUrl}/${mentorRouletteLogId}`);
+	}
+
+	public create(mentorRouletteLog: MentorRouletteLogModel): Observable<MentorRouletteLogModel> {
+		return this._http.post<MentorRouletteLogModel>(`${this._baseUrl}`, mentorRouletteLog);
+	}
+	
+	public update(mentorRouletteLog: MentorRouletteLogModel): Observable<MentorRouletteLogModel> {
+		return this._http.put<MentorRouletteLogModel>(`${this._baseUrl}/${mentorRouletteLog.mentorRouletteLogId}`, mentorRouletteLog);
+	}
+	
+	public delete(mentorRouletteLogId: number): Observable<void> {
+		return this._http.delete<void>(`${this._baseUrl}/${mentorRouletteLogId}`);
+	}
 }
